e84a2e7f8ac626cb51ea150d1c0bf1e625e01dfd
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-09-20 18:34:48 +0000

    added file assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const assertArraysEqual = function (actualArr, expectedArr) {
+  if (!(actualArr.length === expectedArr.length)) {
+    console.log(`🛑🛑🛑 Assertion Failed: length of ${actualArr} !== length of ${expectedArr}`);
+    return;
+  }
+  for (let i = 0; i < actualArr.length; i++) {
+    if (!(actualArr[i] === expectedArr[i])) {
+      console.log(`🛑🛑🛑 Assertion Failed at index ${i}: ${actualArr} !== ${expectedArr}`);
+      return;
+    }
+  }
+  console.log(`🟢🟢🟢 Assertion Passed: ${actualArr} === ${expectedArr}`);
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [3, 2, 1])
+assertArraysEqual(["1", "2", "3", '4'], ["1", "2", "3"])
+assertArraysEqual(["1", "2", "3"], ["1", "2", '3', '4'])

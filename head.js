14416ab0acbad53bac186d7854f29c2cb8b41ac8
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-09-27 20:40:52 +0000

    added test files for all lotide functions and refactored assertEqual and head for exports compatibility

--- head.js
+++ head.js
@@ -2,14 +2,5 @@ const head = function(array) {
   return array[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([5]), 5);
-assertEqual(head([]), "Hello");
+module.exports = head;
 
---
+++ test/findKeyTest.js
@@ -0,0 +1,39 @@
+//TESTING
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function (object, criteria) {
+  //look through object keys
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      if (criteria(object[key])) {
+        return key;
+      }
+    }
+  }
+  return 'Key not found';
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), 'noma');
